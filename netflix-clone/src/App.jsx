82538b5d61258b4059e7a0e1abd96a0263865e29
ephremdoyo <ephremdoyo@gmail.com>
2024-09-09 01:44:56 +0300

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
     return (
-    <>
-     <h1>Test</h1>       
-    </>
+    <div className="App">
+    <Home/>     
+    </div>
   )
 }
 
